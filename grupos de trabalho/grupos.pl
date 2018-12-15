ad25d1f96ae0caa0105b3b0e7750b6402554f0f3
Ana Rita Norinho <ritanorinho98@hotmail.com>
2018-12-15 12:17:19 +0000

    só falta a restrição das médias
    testado com dimensões de grupos diferentes e corrigidos alguns erros nas outras restrições. Penso que agora estejam bem

--- grupos de trabalho/grupos.pl
+++ grupos de trabalho/grupos.pl
@@ -5,6 +5,7 @@
 
 class1([1,1,1,1,2,2,2,2,3,4,3,4,3,4,3,4]).
 class2([1,2,1,2,2,2,3,1,1,3,3,4,4,4,3,4]).
+class3([1,1,1,2,2,2]).
 
 
 generate_grades([],0,FinalList,SortedList,Minimum,Maximum):- samsort(FinalList,SortedList).
@@ -34,13 +35,13 @@ groups(NumberStudents):-class1(X),
                         list_global(Groups3,NumberGroups,4,List,FinalList),
                         global_cardinality(Groups1,FinalList),
                         count_repetitions(Groups1,Acc,X),
-                        labeling([minimize(Acc)],Groups1),write(X),nl,write(Groups1),nl,
+                        labeling([minimize(Acc)],Groups1),write(Acc),nl,write(X),nl,write(Groups1),nl,
                         length(Groups2,NumberStudents),
                         domain(Groups2,1,NumberGroups),
                         global_cardinality(Groups2,FinalList),
                         count_repetitions(Groups2,Acc1,X),
                         go_through_class(Groups2,Groups1),
-                        labeling([minimize(Acc1)],Groups2),write(Groups2). 
+                        labeling([minimize(Acc1)],Groups2),write(Acc1),nl,write(X),nl,write(Groups2). 
 
 
 
@@ -53,29 +54,28 @@ list_global(CurrPosition,NumberGroups,NumberElements,CurrList,FinalList):-
 
 
 go_through_class([_],_).
-go_through_class([H|T],X):-duos([H|T],2,X),
-                           go_through_class(T,X).
-
-duos([_,_],Index,_).
-duos([A,B,C|T],Index,X):- 
-                      element(1,X,Val1),
-                      element(Index,X,Val2),
-                      Index1 #= Index + 1,
-                      ((A #\= B) #\/ Val1 #\= Val2),
-                      duos([A,C|T],Index1,X).
+go_through_class([H|T],[X1|X]):-go_through_class(T,X),
+                                duos([H|T],[X1|X]).
 
 
-count_repetitions([_],0,X).
+duos([A,B,C|T],[X1,X2,X3|X]):-
+                      ((A #\= B) #\/ X1 #\= X2),
+                      duos([A,C|T],[X1,X3|X]).
 
-count_repetitions([H|T],Acc,X):- repetitions([H|T],X,Acc1,1),
-                                 count_repetitions(T,Acc2,X),
+
+duos([A,B],[X1,X2]):-((A #\= B) #\/ X1 #\= X2).
+
+count_repetitions([_],0,[_]).
+
+count_repetitions([H|T],Acc,[X1|X]):- count_repetitions(T,Acc2,X), 
+                                 repetitions([H|T],[X1|X],Acc1),
                                  Acc #= Acc2+Acc1.
-repetitions([_,_],_,0,_).
-repetitions([A,B,C|T],X,Acc,Index):-     Index1 #= Index + 1,                                         
-                                         repetitions([A,C|T],X,Acc1,Index1),
-                                         element(1,X,Val1),
-                                         element(Index1,X,Val2),
-                                         (Val1 #= Val2 #/\ A #=B)#<=>Bin,
+
+repetitions([A,B],[X1,X2],Acc):- (X1 #= X2 #/\ A #=B)#<=>Bin, Acc #= Bin.
+repetitions([A,B,C|T],[X1,X2,X3|X],Acc):-                                             
+                                         repetitions([A,C|T],[X1,X3|X],Acc1),
+                                         (X1 #= X2 #/\ A #=B)#<=>Bin,
+                                         %write('['),write(X1),write(','), write(X2),write(']'),nl,
                                          Acc #= Acc1 + Bin.
 
 
@@ -85,8 +85,4 @@ calculate_number_groups(Students,NumberGroups4,NumberGroups3):-
                   NumberGroups3 in 0..Students,
                   NumberGroups4 in 0..Students,
                   Students #= NumberGroups3 * 3 + NumberGroups4 * 4,
-                  NumberGroups3 #=< NumberGroups4,
-                  labeling([],Values).
-
-                                          
-                                           
+                  labeling([maximize(NumberGroups4)],Values).
