07674d100b6bd13f79ba69f65634e5edf7810e9e
Ana Rita Norinho <ritanorinho98@hotmail.com>
2018-12-15 18:20:03 +0000

    calcula as médias por grupo, só falta a restrição

--- grupos de trabalho/grupos.pl
+++ grupos de trabalho/grupos.pl
@@ -5,16 +5,16 @@
 
 class1([1,1,1,1,2,2,2,2,3,4,3,4,3,4,3,4]).
 class2([1,2,1,2,2,2,3,1,1,3,3,4,4,4,3,4]).
-class3([1,1,1,2,2,2]).
+class3([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]).
 
 
-generate_grades([],0,FinalList,SortedList,Minimum,Maximum):- samsort(FinalList,SortedList).
-generate_grades([H|T],Index,FinalList,SortedList,Minimum,Maximum):-
+generate_grades(0,FinalList,FinalList).
+generate_grades(Index,CurrList,FinalList):-
                   Index > 0,
                   Index1 is Index-1,
-                  random(Minimum,Maximum,H),
-                  append(FinalList,[H],FinalList1),
-                  generate_list(T,Index1,FinalList1,SortedList,Minimum,Maximum).
+                  random(10,20,H),
+                  append(CurrList,[H],CurrList1),
+                  generate_grades(Index1,CurrList1,FinalList),!.
 generate_groups([],0,_,Minimum,Maximum).
 generate_groups([H|T],Index,FinalList,Minimum,Maximum):-
                   Index > 0,
@@ -27,49 +27,57 @@ analyze_groups(Groups,Grades,NumberGroups).
 
 
 groups(NumberStudents):-class1(X),
+                        generate_grades(NumberStudents,CurrGrades,ListGrades),write(ListGrades),nl,
                         length(Groups1,NumberStudents),
                         calculate_number_groups(NumberStudents,Groups4,Groups3), 
                         NumberGroups #= Groups3+Groups4,
                         domain(Groups1,1,NumberGroups),
-                        list_global(0,Groups3,3,[],List),
-                        list_global(Groups3,NumberGroups,4,List,FinalList),
+                        list_global(0,Groups3,3,[],List,[],CurrentGroups),
+                        list_global(Groups3,NumberGroups,4,List,FinalList,CurrentGroups,FinalGroups),
                         global_cardinality(Groups1,FinalList),
-                        count_repetitions(Groups1,Acc,X),
-                        labeling([minimize(Acc)],Groups1),write(Acc),nl,write(X),nl,write(Groups1),nl,
+                        count_repetitions(Groups1,Acc,X),length(AverageGrades,NumberGroups),
+                        calculate_average_grades(Groups1,ListGrades,FinalGroups,1,NumberGroups,AverageGrades),
+                        labeling([minimize(Acc)],Groups1),write(AverageGrades),nl,write(Acc),nl,write(X),nl,write(Groups1),nl,
                         length(Groups2,NumberStudents),
                         domain(Groups2,1,NumberGroups),
                         global_cardinality(Groups2,FinalList),
                         count_repetitions(Groups2,Acc1,X),
-                        go_through_class(Groups2,Groups1),
-                        labeling([minimize(Acc1)],Groups2),write(Acc1),nl,write(X),nl,write(Groups2). 
+                        count_repetitions(Groups2,0,Groups1),length(AverageGrades1,NumberGroups),
+                        calculate_average_grades(Groups2,ListGrades,FinalGroups,1,NumberGroups,AverageGrades1),
+                        labeling([minimize(Acc1)],Groups2),write(AverageGrades1),nl,write(Acc1),nl,write(X),nl,write(Groups2). 
 
 
 
-list_global(CurrPosition,NumberGroups,NumberElements,FinalList,FinalList):- CurrPosition >= NumberGroups.
-list_global(CurrPosition,NumberGroups,NumberElements,CurrList,FinalList):-
+list_global(CurrPosition,NumberGroups,NumberElements,FinalList,FinalList,ListGroups,ListGroups):- CurrPosition >= NumberGroups.
+list_global(CurrPosition,NumberGroups,NumberElements,CurrList,FinalList,CurrentGroups,FinalGroups):-
                                                            CurrPosition < NumberGroups,                                          
                                                            CurrPosition1 is CurrPosition+1,
                                                            append(CurrList,[CurrPosition1-NumberElements],CurrList1),
-                                                           list_global(CurrPosition1,NumberGroups,NumberElements,CurrList1,FinalList).
+                                                           append(CurrentGroups,[NumberElements],CurrentGroups1),
+                                                           list_global(CurrPosition1,NumberGroups,NumberElements,CurrList1,FinalList,CurrentGroups1,FinalGroups).
 
 
-go_through_class([_],_).
-go_through_class([H|T],[X1|X]):-go_through_class(T,X),
-                                duos([H|T],[X1|X]).
 
+calculate_average_grades(List,Grades,GroupElements,CurrentGroup,NumberGroups,[]).
+calculate_average_grades(List,Grades,[G1|G],CurrentGroup,NumberGroups,[H|T]):-    
+                                                                            sum_grades(List,Grades,CurrentGroup,Sum),
+                                                                            H #= Sum / G1,
+                                                                            CurrentGroup1 #= CurrentGroup+1,
+                                                                            calculate_average_grades(List,Grades,G,CurrentGroup1,NumberGroups,T).
 
-duos([A,B,C|T],[X1,X2,X3|X]):-
-                      ((A #\= B) #\/ X1 #\= X2),
-                      duos([A,C|T],[X1,X3|X]).
+sum_grades([A],[G1],Group,SumGrades):- (A #= Group) #<=> Bin,
+                                       SumGrades #= G1 * Bin.
 
+sum_grades([A,B|T],[G1,G2|G],Group,SumGrades):-       sum_grades([B|T],[G2|G],Group,Sum),
+                                                      (A #= Group) #<=> Bin,
+                                                      SumGrades #= Sum + G1 * Bin.
 
-duos([A,B],[X1,X2]):-((A #\= B) #\/ X1 #\= X2).
 
 count_repetitions([_],0,[_]).
 
 count_repetitions([H|T],Acc,[X1|X]):- count_repetitions(T,Acc2,X), 
-                                 repetitions([H|T],[X1|X],Acc1),
-                                 Acc #= Acc2+Acc1.
+                                      repetitions([H|T],[X1|X],Acc1),
+                                      Acc #= Acc2+Acc1.
 
 repetitions([A,B],[X1,X2],Acc):- (X1 #= X2 #/\ A #=B)#<=>Bin, Acc #= Bin.
 repetitions([A,B,C|T],[X1,X2,X3|X],Acc):-                                             
@@ -86,3 +94,4 @@ calculate_number_groups(Students,NumberGroups4,NumberGroups3):-
                   NumberGroups4 in 0..Students,
                   Students #= NumberGroups3 * 3 + NumberGroups4 * 4,
                   labeling([maximize(NumberGroups4)],Values).
+
